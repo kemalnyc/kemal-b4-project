7c9346218c8003c901507e1034183221fecbdac9
kemalnyc <kemalnyc@gmail.com>
2021-08-29 09:52:06 -0400

    Gitday2 updated

--- src/com/company/GitDay2.java
+++ src/com/company/GitDay2.java
@@ -4,4 +4,8 @@ public class GitDay2 {
     public static void main(String[] args) {
         System.out.println("Good morning! It's Git Day 2");
     }
+
+    public static String revStr(String str){
+        return new StringBuilder(str).reverse().toString();
+    }
 }
